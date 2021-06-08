a908b2eea97558888f3a15d85e615e7670b36129
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-07 18:08:25 -0600

    feat: :sparkles: finish singleton status pattern

--- lib/pages/page1_page.dart
+++ lib/pages/page1_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:status/models/user.dart';
+import 'package:status/services/user_service.dart';
 
 class Page1Page extends StatelessWidget {
   @override
@@ -7,10 +9,57 @@ class Page1Page extends StatelessWidget {
         appBar: AppBar(
           title: Text('Page 1'),
         ),
-        body: Center(child: Text('Page 1')),
+        body: StreamBuilder(
+          stream: userService.userStream,
+          builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+            return snapshot.hasData
+                ? UserInfo(snapshot.data)
+                : Center(
+                    child: Text('There is no user info.'),
+                  );
+          },
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () => Navigator.pushNamed(context, 'page2'),
           child: Icon(Icons.arrow_forward_rounded),
         ));
+  }
+}
+
+class UserInfo extends StatelessWidget {
+  final User user;
+
+  UserInfo(this.user);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'General',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Divider(),
+          ListTile(title: Text('Name: ${user.name}')),
+          ListTile(title: Text('Age: ${user.age}')),
+          Text(
+            'Professions',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Divider(),
+          ...List.generate(
+                  user.professions.length,
+                  (index) => ListTile(
+                      title: Text(
+                          'Profession ${index + 1}: ${user.professions[index]}')))
+              .toList()
+        ],
+      ),
+    );
   }
 }
--- lib/pages/page2_page.dart
+++ lib/pages/page2_page.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:status/models/user.dart';
+import 'package:status/services/user_service.dart';
 
 class Page2Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Page 2'),
+          title: StreamBuilder(
+            stream: userService.userStream,
+            builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+              return Text(snapshot.hasData ? snapshot.data.name : 'Page 2');
+            },
+          ),
         ),
-        body: Center(child: Text('Page 2')),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(
+              onPressed: () {
+                userService.user = new User(
+                    age: 21, name: 'Uriel', professions: ['Systems Engineer']);
+              },
+              color: Colors.blueAccent,
+              child: Text('Set user', style: TextStyle(color: Colors.white)),
+            ),
+            MaterialButton(
+              onPressed: () {
+                userService.changeAge(22);
+              },
+              color: Colors.blueAccent,
+              child: Text('Change age', style: TextStyle(color: Colors.white)),
+            ),
+            MaterialButton(
+              onPressed: () {},
+              color: Colors.blueAccent,
+              child: Text('Add profession ',
+                  style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        )),
         floatingActionButton: FloatingActionButton(
           onPressed: () => Navigator.pop(context),
           child: Icon(Icons.arrow_back_rounded),
